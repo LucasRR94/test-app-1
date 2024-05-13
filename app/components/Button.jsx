a8b157d67d113a50a919ab976e1325291d8f06a1
Lucas Rocha <lucasregodarocha@outlook.com>
2024-05-12 21:30:50 -0300

    fix: fixed style on button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -10,8 +10,8 @@ export default function Button({
     <button
       className={
         invertedColorScheme
-          ? "flex bg-[#7357FF] text-white border-t-[1px] py-[12px] px-[16px] rounded-xl shadow-md ring-1 ring-offset-0 ring-offset-gray-200 ring-opacity-50 gap-y-2.5 font-sans text-base font-semibold leading-6 text-left items-center"
-          : "flex text-[#7357FF] bg-[#F9F8FF] border-t-[1px] py-[12px] px-[16px] hover:bg-purple-600 py-2 px-4 rounded-xl shadow-md gap-y-2.5 font-sans text-base font-semibold leading-6 text-left items-center"
+          ? "flex bg-[#7357FF] text-white border-t-[1px] hover:bg-white hover:text-[#7357FF] py-[12px] px-[16px] rounded-xl shadow-md ring-1 ring-offset-0 ring-offset-gray-200 ring-opacity-50 gap-y-2.5 font-sans text-base font-semibold leading-6 text-left items-center"
+          : "flex text-[#7357FF] bg-[#F9F8FF] border-t-[1px] py-[12px] px-[16px] hover:bg-purple-600 hover:text-white py-2 px-4 rounded-xl shadow-md gap-y-2.5 font-sans text-base font-semibold leading-6 text-left items-center"
       }
       onClick={onClick}
     >
